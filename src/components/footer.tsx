19359e0b8b65d98706ab0531e01727c1a2b3513f
max180643 <max180643@gmail.com>
2021-03-28 03:05:06 +0700

    :lipstick: Edit icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Github } from './icons/index'
+import { Github, Linkedin } from './icons/index'
 
 export const Footer: React.FC = (props) => {
   return (
     <footer>
       <div className='mt-10 mb-5 flex max-w-full'>
         <div className='flex ml-auto'>
-          <Github />
+          <a href='https://www.linkedin.com/in/max180643/' className='text-gray-300 hover:text-primary mx-1'>
+            <Linkedin size={10} />
+          </a>
+          <a href='https://github.com/max180643' className='text-gray-300 hover:text-primary mx-1'>
+            <Github size={10} />
+          </a>
         </div>
       </div>
     </footer>
