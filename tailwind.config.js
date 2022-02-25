db2a9bcede45284ab9fecd8d5c209228379459ac
max180643 <max180643@gmail.com>
2022-02-25 20:25:11 +0700

    :wrench: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,8 @@ export default {
       },
     },
   },
-  purge: ["./index.html",'./src/**/*.{svelte,js,ts}'], // for unused CSS
+  content: ["./index.html",'./src/**/*.{svelte,js,ts}'], // for unused CSS
   variants: {
     extend: {},
   },
-  darkMode: false,
 }
